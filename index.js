dce5db539fb21b6d2479786960bd2c35c5316f1b
Danny <142972494+DannyAkintunde@users.noreply.github.com>
2024-09-26 00:53:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const path = require('path');
 const config = require('./config');
+const http = require('http');
+const PORT = Number(process.env.PORT) || 8000
 const { requireJS, retrivePlugins, client } = require('./lib');
 async function initialize() {
+ const s = http.createServer((req,res)=>{res.write("bot on");res.end()});
+ s.listen(PORT, ()=>console.log("server on"));
  await requireJS(path.join(__dirname, '/lib/Client/database/'));
  console.log('Syncing Database');
  await config.DATABASE.sync();
